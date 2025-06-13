ecd41c63ed5dcb3579ef546b2fd4e1471a971be3
lucas <santanafx@hotmail.com>
2025-06-13 07:24:48 -0300

    feat: Integrate notifications into Home component using custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { Header } from "@/components/organisms/header";
+import { useGetNotifications } from "@/services/hooks/useGetNotifications";
 import { useState } from "react";
 
 export default function Home() {
+  const notifications = useGetNotifications();
   const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
   const [isProjectModalOpen, setIsProjectModalOpen] = useState(false);
 
   return (
     <div className="min-h-screen bg-gray-50 flex">
       <Header
+        notifications={notifications}
         onCreateTask={() => setIsTaskModalOpen(true)}
         onCreateProject={() => setIsProjectModalOpen(true)}
       />
