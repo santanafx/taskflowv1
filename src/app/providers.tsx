dcdcbdf647cc00ba0155df1bd46bb69d47714562
lucas <santanafx@hotmail.com>
2025-06-18 12:38:24 -0300

    feat: Integrate Redux Provider into the Providers component for enhanced state management

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { store } from "@/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { Provider } from "react-redux";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+    </QueryClientProvider>
   );
 }
